256b6017f42e41e61477f0e729f56dfd2b5d4406
Pavel Saman <pavelsam@centrum.cz>
2020-12-08 22:48:43 +0100

    Fix test

--- tests/first.test.js
+++ tests/first.test.js
@@ -12,7 +12,7 @@ const expect = chai.expect;
 suite('first()', function () {
 
     test('empty array', function () {
-        expect(useful.first([])).to.equal(null);
+        expect(useful.first([])).to.equal(undefined);
     });
 
     test('non-empty array, one element', function () {
